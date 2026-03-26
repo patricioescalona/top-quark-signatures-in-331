ad36001fec91a739ad09183e03ba7794bacd5ecc
Patricio <patricioescalona96@gmail.com>
2026-03-25 21:14:00 -0300

    Added UFO for model with arbitrary constants

---
+++ top-pseudoscalar_UFO/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 13.2.1 for Linux x86 (64-bit) (January 28, 2023)
+# Date: Wed 25 Mar 2026 21:08:30
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
