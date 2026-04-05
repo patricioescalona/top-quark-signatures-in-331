acd29bb9ef1fd9c3c674f36822b514ec4f9c7f66
Patricio <patricioescalona96@gmail.com>
2026-04-04 23:45:20 -0300

    Updated model generation: variant I and 3 different benchmarks

---
+++ model-generation/top-pseudoscalar-varI-BM1_UFO/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 13.2.1 for Linux x86 (64-bit) (January 28, 2023)
+# Date: Sat 4 Apr 2026 23:39:33
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
---
+++ model-generation/top-pseudoscalar-varI-BM2_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 13.2.1 for Linux x86 (64-bit) (January 28, 2023)
+# Date: Sat 4 Apr 2026 23:41:33
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
